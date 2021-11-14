cc108f2a49ac9e01f7ff80aee8311466db2663f0
appleapplegaming <abou08923@gmail.com>
2021-11-14 19:47:49 +0100

    changed calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,13 @@
-a = int(input("enter first number: "))
-b = int(input("enter second number: "))
-sum = a + b
-print("sum:", sum)
+lol = input("What do you want to do?")
+
+if lol == "add":
+    a = int(input("enter first number: "))
+    b = int(input("enter second number: "))
+    sum = a + b
+    print("sum:", sum)
+    
+if lol == "minus":
+    aminus = int(input("enter first number: "))
+    bminus = int(input("enter second number: "))
+    summinus = aminus - bminus
+    print("sum:", summinus)
